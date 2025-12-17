10ce7e48376d1551d1eef7782f381bb0a28063b4
wind010 <wind010@users.noreply.github.com>
2025-12-16 16:03:25 -0800

    Create parent directories if they don't exist when creating the symlinks

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -10,9 +10,15 @@ LINK_MAP["$HOME/.config/nvim"]="nvim"
 LINK_MAP["$HOME/.config/zsh/.zshrc"]=".zshrc"
 LINK_MAP["$HOME/.config/tmux/tmux.conf"]="tmux.conf"
 
-
 for LINK_PATH in "${!LINK_MAP[@]}"; do
     TARGET_PATH="$GIT_REPO_FOR_DOTFILES/${LINK_MAP[$LINK_PATH]}"
+    PARENT_DIR=$(dirname "$LINK_PATH")
+
+    # Ensure the parent directory exists
+    if [ ! -d "$PARENT_DIR" ]; then
+        echo "📂 Creating parent directory: $PARENT_DIR"
+        mkdir -p "$PARENT_DIR"
+    fi
 
     if [ -e "$LINK_PATH" ] || [ -L "$LINK_PATH" ]; then
         echo "$LINK_PATH already exists... recreating symlink."
